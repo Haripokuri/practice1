90696ecf446ba3ddeec7516c0e075771290e654e
Haripokuri <101180391+Haripokuri@users.noreply.github.com>
2022-10-07 15:26:46 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,5 @@ provider "aws" {
 # main configuration
 resource "aws_instance" "ubuntu" {
 ami = "ami-03f4fa076d2981b45"
-access_key = "AKIAT57VKV53KOTZF7ED"
-secret_key = "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"
 instance_type = "t2.micro"
 }
